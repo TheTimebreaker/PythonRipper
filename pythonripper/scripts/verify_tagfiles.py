39bd092c49d216902a5e13147e030c688343bde6
sharkie <git@thetimebreaker.mozmail.com>
2026-04-11 00:41:39 +0200

    minor

--- pythonripper/scripts/verify_tagfiles.py
+++ pythonripper/scripts/verify_tagfiles.py
@@ -1,21 +1,25 @@
+import asyncio
+
 import pythonripper.toolbox.centralfunctions as cf
 import pythonripper.toolbox.config as cfg
 import pythonripper.toolbox.subscription_management as sm
 
 
-def main(config: cfg.Config) -> None:
+async def main(config: cfg.Config) -> None:
     print("=" * 20)
     print("Artist file")
 
-    _obj = sm.CombinedArtistFile(config)
+    obj = sm.CombinedArtistFile(config)
+    await obj.write()
 
     print("=" * 20)
     print("Booru file")
 
-    _obj2 = sm.CombinedBooruFile(config)
+    obj2 = sm.CombinedBooruFile(config)
+    await obj2.write()
 
 
 if __name__ == "__main__":
     config = cfg.Config()
     cf.init_logger(config, "warning", False)
-    main(config)
+    asyncio.run(main(config))
